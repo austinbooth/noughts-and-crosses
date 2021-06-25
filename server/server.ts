6ccf0cdc5461b40597b0f67c7e61e25c7a41c39b
Austin Booth <austinbooth@austin.local>
2021-06-25 18:47:16 +0100

    sets up socket io

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+const http = require('http');
+const { Server, Socket } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const PORT = 9090;
+
 app.get('/', (req, res) => res.send('Noughts and crosses game server'));
-app.listen(PORT, () => {
+
+io.on('connection', (socket: typeof Socket) => {
+  console.log('a user connected');
+});
+
+server.listen(PORT, () => {
   console.log(`[server]: Server is running at https://localhost:${PORT}`);
 });
