a541ab74fc0d01e8d52d02c722d6838532f88d07
Austin Booth <austinbooth@austin.local>
2021-06-26 23:14:48 +0100

    stops auto connecting and stores socket in state - preparing for connecting to a room

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,25 @@ import { store } from './types/store'
 import { GameSetup } from './components/GameSetup'
 import { Game } from './components/Game'
 import { WinningMsg } from './components/WinningMsg'
-import socketIOClient from "socket.io-client";
+import socketIOClient, { Socket } from "socket.io-client";
 
-const ENDPOINT = "http://127.0.0.1:9090"; // TODO store in environment variable
+const URL = "http://127.0.0.1:9090"; // TODO store in environment variable
 
 const App = observer(props => {
   const { board, game_in_play, winner } = store
 
   const [data, setData] = useState('')
+  const [socket, setSocket] = useState<Socket>()
 
-  useEffect((): () => void => {
-    const socket = socketIOClient(ENDPOINT)
+  useEffect((): (() => void) => {
+    const socket = socketIOClient(URL, { autoConnect: false })
     socket.on("FromServer", data => setData(data))
+
+    socket.onAny((event, ...args) => {
+      console.log(event, args);
+    })
+
+    setSocket(socket)
 
     return () => socket.disconnect();
   }, [])
